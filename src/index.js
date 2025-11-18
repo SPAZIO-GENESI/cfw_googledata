4ac3492e9ea9a3ff236c2cf8460a9ce0d448e755
maxAlone <mascam64@gmail.com>
2025-11-18 15:04:07 +0100

    debug con log forzati

--- src/index.js
+++ src/index.js
@@ -7,32 +7,19 @@ export default {
     console.log("Incoming request:", url.toString())
     console.log("Sheet:", sheetId, "f:", f)
 
-    if (!sheetId) return new Response("Missing sheet parameter", { status: 400 })
-    if (!f) return new Response("Missing f parameter", { status: 400 })
-
     const secretName = `GOOGLE_API_KEY_${f}`
     const apiKey = env[secretName]
 
     console.log("Secret name:", secretName, "Found:", !!apiKey)
 
-    if (!apiKey) return new Response(`Missing secret ${secretName}`, { status: 500 })
-
-    const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Disponibili?key=${apiKey}`
-    console.log("Calling Sheets API:", sheetUrl)
-
     try {
-      const response = await fetch(sheetUrl)
+      const response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Disponibili?key=${apiKey}`
+      )
       const data = await response.json()
-      console.log("Sheets API response:", JSON.stringify(data).slice(0, 200)) // log primi 200 caratteri
+      console.log("Sheets API response sample:", JSON.stringify(data).slice(0, 200))
 
-      return new Response(JSON.stringify(data), {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          "Access-Control-Allow-Headers": "*"
-        }
-      })
+      return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } })
     } catch (err) {
       console.error("Error fetching Sheets:", err)
       return new Response(JSON.stringify({ error: err.message }), { status: 500 })
